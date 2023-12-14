268ec7ba499d6d5817ea3101f8f47cf537253da9
Akshat Sharma <akshatsharma2602@gmail.com>
2023-12-14 13:18:55 -0500

    Fixed requirements

--- coverweaver.py
+++ coverweaver.py
@@ -1,3 +1,4 @@
+import sys
 import streamlit as st
 import json
 from langchain.chat_models import ChatOpenAI
@@ -11,6 +12,10 @@ from langchain.docstore.document import Document
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+__import__('pysqlite3')
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 def init_db():
